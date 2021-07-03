0d09217a45a3d3964af357d544b55e0f3b69ac98
ayukin <uf1ohdpwgm@gmail.com>
2021-07-04 04:26:35 +0900

    Initial Commit

---
+++ MatchingApp/Controllers/HomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MatchingApp
+//
+//  Created by 720.nishioka on 2021/07/04.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        
+        let view1 = UIView()
+        view1.backgroundColor = UIColor.red
+        
+        let view2 = UIView()
+        view2.backgroundColor = UIColor.blue
+        
+        let view3 = UIView()
+        view3.backgroundColor = UIColor.green
+        
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        self.view.addSubview(stackView)
+                
+        NSLayoutConstraint.activate([
+            view1.heightAnchor.constraint(equalToConstant: 100),
+            view3.heightAnchor.constraint(equalToConstant: 120),
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+        
+    }
+
+
+}
+
